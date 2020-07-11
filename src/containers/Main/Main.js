a45527489ac5daff941663c7b1ef868211814d7b
Alexander Johnston <xanderjohnston2@gmail.com>
2020-07-11 13:25:56 +0100

    job handling loic

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,17 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import API from '../../adapters/API'
-import Map from '../../components/Map/Map'
+import JobMap from '../../components/JobMap/JobMap'
+import Task from '../../components/Task/Task'
 
 const Main = () => {
 
   const [jobs, setJobs] = useState()
+  const [currentJob, setCurrentJob] = useState(null)
+  const [acceptedJob, setAcceptedJob] = useState(null)
 
   useEffect(() => {
     API.getAllJobs()
     .then(res => setJobs(res))
   }, [])
 
-  return jobs ? <Map jobs={jobs} /> : 'Loading...'
+  const viewJob = (job) => {
+    setCurrentJob(job)
+  }
+
+  const acceptJob = (job) => {
+    setAcceptedJob([job])
+  }
+
+  const validateCoords = (coords) => {
+    return (coords.longitude <= 180 && coords.longitude >= -180) && (coords.latitude <= 90 && coords.latitude >= -90)
+  }
+
+  const jobsToShow = jobs && (acceptedJob ? acceptedJob : jobs.filter(j => validateCoords(j.$propertyLocation.coords)))
+
+  return jobs ?
+          <>
+          <JobMap jobs={jobsToShow} viewJob={viewJob} currentJob={currentJob} acceptJob={acceptJob}/> 
+          </>
+          : 'Loading...'
 }
 
 export default Main
