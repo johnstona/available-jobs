3366c405a5b5f61522e732a177becb6019f14248
Alexander Johnston <xanderjohnston2@gmail.com>
2020-07-11 13:25:38 +0100

    added react jobmap and markers

---
+++ src/components/JobMap/JobMap.js
@@ -0,0 +1,56 @@
+import React, { useState, useRef } from 'react'
+import ReactMapboxGl, { Marker, Popup } from "react-mapbox-gl";
+import './jobmap.css'
+
+
+const JobMap = ({jobs, viewJob, currentJob, acceptJob }) => {
+
+  const jobMap = useRef(null)
+    
+  const Map = ReactMapboxGl({
+    accessToken: process.env.REACT_APP_API_KEY
+  });
+
+  const handleClick = (job) => {
+    debugger
+    jobMap.current.state.map.flyTo({center: [job.$propertyLocation.coords.longitude, job.$propertyLocation.coords.latitude]})
+    viewJob(job)
+  }
+
+  const handleAccept = () => {
+    acceptJob(currentJob)
+  } 
+
+  return  <Map ref={jobMap}
+            style="mapbox://styles/mapbox/streets-v9"
+            containerStyle={{
+              height: "100vh",
+              width: "100vw",
+            }}
+            center={[-99.9018131, 31.9685988]}
+            zoom={[2]}
+          >
+
+          {jobs.map(j => {
+                return <Marker coordinates={[j.$propertyLocation.coords.longitude, j.$propertyLocation.coords.latitude]} anchor="center" >
+                          <div className="marker" onClick={() => handleClick(j)}>
+                            <span></span>
+                            {/* <img src="http://www.clker.com/cliparts/g/R/z/I/u/o/map-pin-hi.png"/> */}
+                            </div>
+                        </Marker>
+              })}
+          {currentJob && 
+            <Popup
+            coordinates={[currentJob.$propertyLocation.coords.longitude, currentJob.$propertyLocation.coords.latitude]}
+           >
+            <p>{currentJob.$claims[0].claimType}</p>
+            <button onClick={handleAccept}>Accept job?</button>
+          </Popup>
+          }
+        </Map>
+       
+}
+
+export default JobMap
+
+
