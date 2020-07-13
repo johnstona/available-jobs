22396c8ff18adc81787acf7de0754a27f3bf88a7
Alexander Johnston <xanderjohnston2@gmail.com>
2020-07-13 11:15:28 +0100

    info component added

---
+++ src/components/Info/Info.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './info.css'
+
+const Info = ({acceptedJob, cancel}) => {
+
+  return <div className="info">
+
+            {!acceptedJob ? 
+            <p>Welcome back, please select a job</p>
+                    :
+            <>
+            <p>Job accepted: {acceptedJob[0].$claims[0].claimType}</p>
+            <button onClick={cancel}>Cancel job</button>
+            </>
+            }
+
+          </div>
+
+}
+
+export default Info
